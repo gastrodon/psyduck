3906be4a3a31819b4bc9c419a5cda8a4bf7f4932
Eva <mail@gastrodon.io>
2021-12-13 03:32:28 -0500

    Create config loader
    A funky config loader that looks at arguments and the environment

--- configure.ts
+++ configure.ts
@@ -1,7 +1,52 @@
-import parser from "args-parser";
+const parser = require("args-parser");
 
-export default () => {
+export const enum Config {
+  Job,
+  SourceKind,
+  SourceID,
+  PerSecond,
+  ExitAfter,
+}
+
+const names: Map<Config, string> = new Map([
+  [Config.Job, "job"],
+  [Config.SourceKind, "source-kind"],
+  [Config.SourceID, "source-id"],
+  [Config.PerSecond, "per-second"],
+  [Config.ExitAfter, "exit-after"],
+]);
+
+const defaults: Map<Config, any> = new Map([
+  [Config.PerSecond, 20],
+]);
+
+const iterate = <T>(iterable: Iterable<T>): Array<T> => {
+  let buffer = new Array();
+
+  for (let it of iterable) {
+    buffer.push(it);
+  }
+
+  return buffer;
+};
+
+const as_env = (key: string): string => key.replace("-", "_").toUpperCase();
+
+// TODO validate arguments
+export const configure = (): Map<Config, any> => {
   const args = parser(process.argv);
 
-  console.log(args);
+  return new Map(
+    iterate(names.entries()).map((
+      [kind, name],
+    ) => (
+      [
+        kind,
+        args[name] ??
+          process.env[as_env(name)] ??
+          defaults.get(kind) ??
+          null,
+      ]
+    )),
+  );
 };
