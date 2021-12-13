fcc18037fbc79ec45f93c18f54865fd2e68b1ac8
Eva <mail@gastrodon.io>
2021-12-13 03:43:59 -0500

    Include ifunny email and password config options

--- configure.ts
+++ configure.ts
@@ -1,11 +1,15 @@
 const parser = require("args-parser");
 
+const ENVIRONMENT_PREFIX: string = "IFUNNY_ETL_";
+
 export const enum Config {
   Job,
   SourceKind,
   SourceID,
   PerSecond,
   ExitAfter,
+  Email,
+  Password,
 }
 
 const names: Map<Config, string> = new Map([
@@ -14,6 +18,8 @@ const names: Map<Config, string> = new Map([
   [Config.SourceID, "source-id"],
   [Config.PerSecond, "per-second"],
   [Config.ExitAfter, "exit-after"],
+  [Config.Email, "email"],
+  [Config.Password, "password"],
 ]);
 
 const defaults: Map<Config, any> = new Map([
@@ -30,7 +36,9 @@ const iterate = <T>(iterable: Iterable<T>): Array<T> => {
   return buffer;
 };
 
-const as_env = (key: string): string => key.replace("-", "_").toUpperCase();
+const as_env = (key: string): string => {
+  return ENVIRONMENT_PREFIX + key.replace("-", "_").toUpperCase();
+};
 
 // TODO validate arguments
 export const configure = (): Map<Config, any> => {
