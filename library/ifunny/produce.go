1e9d700ad082fb472d24508ec25e701fa25fd2fe
Eva <mail@gastrodon.io>
2022-07-20 14:47:40 -0400

    Add an ifunny library

---
+++ library/ifunny/produce.go
@@ -0,0 +1,27 @@
+package ifunny
+
+import (
+	"github.com/gastrodon/psyduck/model"
+)
+
+func ProduceFeatures(configRaw interface{}) model.Mover {
+	config := configRaw.(IFunnyConfig)
+
+	return func(signal chan string) chan interface{} {
+		data := make(chan interface{}, 32)
+		nextPage := ""
+
+		go func() {
+			for {
+				page := getFeaturesPage(config, nextPage)
+				nextPage = page.Paging.Cursors.Next
+
+				for _, content := range page.Items {
+					data <- content
+				}
+			}
+		}()
+
+		return data
+	}
+}
