ac7e4f08616f837079768b184417acc2b04e1150
Eva <mail@gastrodon.io>
2022-07-21 21:18:03 -0400

    Some prototype code + dependencies

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/gastrodon/psyduck
 
 go 1.18
+
+require (
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,27 +1,26 @@
 package main
 
 import (
-	"github.com/gastrodon/psyduck/core"
-	"github.com/gastrodon/psyduck/library/scyther"
-	"github.com/gastrodon/psyduck/model"
-	"github.com/gastrodon/psyduck/transform"
+	"fmt"
+
+	"github.com/gastrodon/psyduck/config"
 )
 
-const SCYTHER_URL = "https://webhook.site/96cca981-6550-4772-a72d-5c1498105d4a"
-
-var queueSource = scyther.QueueConfig{
-	URL:   SCYTHER_URL,
-	Queue: "foobar",
-}
-
-var queueDest = scyther.QueueConfig{
-	URL:   SCYTHER_URL,
-	Queue: "dest",
-}
+const yaml = `
+pipelines:
+    ifunny-data:
+        producer:
+            kind: ifunny-features
+        consumer:
+            kind: psyduck-trash
+`
 
 func main() {
-	produce := scyther.ProduceQueue(interface{}(queueSource))
-	consume := scyther.ConsumeQueue(interface{}(queueDest))
+	configured, err := config.Load([]byte(yaml))
+	if err != nil {
+		panic(err)
+	}
 
-	core.RunPipeline(produce, consume, []model.Transformer{transform.Inspect})
+	fmt.Printf("%#v\n", configured)
+	fmt.Printf("%#v\n", configured.Pipelines["ifunny-data"])
 }
