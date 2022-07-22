600347573e5f4822e6a67fc687476473ad093182
Eva <mail@gastrodon.io>
2022-07-21 21:15:01 -0400

    Create a pipeline builder + runner

--- core/run.go
+++ core/run.go
@@ -4,19 +4,43 @@ import (
 	"github.com/gastrodon/psyduck/model"
 )
 
-func RunPipeline(producer, consumer model.Mover, transformers []model.Transformer) {
+func stackTransform(transformers []model.Transformer) model.Transformer {
+	if len(transformers) == 0 {
+		return func(data interface{}) interface{} { return data }
+	}
+
+	if len(transformers) == 1 {
+		return transformers[0]
+	}
+
+	return func(data interface{}) interface{} {
+		return transformers[0](stackTransform(transformers[1:])(data))
+	}
+}
+
+func BuildPipeline(descriptor model.PipelineDescriptor, library model.Library) model.Pipeline {
+	producer := library.ProvideProducer(descriptor.Producer.Kind, descriptor.Producer.Config)
+	consumer := library.ProvideConsumer(descriptor.Consumer.Kind, descriptor.Consumer.Config)
+
+	transformers := make([]model.Transformer, len(descriptor.Transformers))
+	for index, transformDescriptor := range descriptor.Transformers {
+		transformers[index] = library.ProvideTransformer(transformDescriptor.Kind, transformDescriptor.Config)
+	}
+
+	return model.Pipeline{
+		Producer:           producer,
+		Consumer:           consumer,
+		Transformers:       transformers,
+		StackedTransformer: stackTransform(transformers),
+	}
+}
+
+func RunPipeline(pipeline model.Pipeline) {
 	signal := make(chan string)
+	chanProducer := pipeline.Producer(signal)
+	chanConsumer := pipeline.Consumer(signal)
 
-	produceChannel := producer(signal)
-	consumeChannel := consumer(signal)
-
-	index := 0
-	for data := range produceChannel {
-		for _, transform := range transformers {
-			data = transform(data)
-		}
-
-		consumeChannel <- data
-		index++
+	for data := range chanProducer {
+		chanConsumer <- pipeline.StackedTransformer(data)
 	}
 }
