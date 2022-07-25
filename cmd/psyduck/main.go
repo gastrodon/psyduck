952dfd7d33573a686073e07efc9a29621098a2ae
Eva <mail@gastrodon.io>
2022-07-24 20:05:00 -0400

    Create a basic interpereter + remove main.go test stuff

--- cmd/psyduck/main.go
+++ cmd/psyduck/main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+
+	"github.com/gastrodon/psyduck/config"
+	"github.com/gastrodon/psyduck/core"
 )
 
 func main() {
-	fmt.Println("psyduck is offline")
+	file := flag.String("file", "psyduck.yml", "File to interpret")
+	flag.Parse()
+
+	fmt.Println(*file)
+
+	pipelinesRaw, err := config.LoadFile(*file)
+	if err != nil {
+		panic(err)
+	}
+
+	library := core.NewLibrary()
+	library.Load(std.IFunny())
+	library.Load(std.Psyduck())
+	library.Load(std.Scyther())
+
+	pipelines := make(core.Pipelines, len(pipelinesRaw))
+	for name, pipelineRaw := range pipelinesRaw {
+		pipelines[name] = core.BuildPipeline(pipelineRaw, library)
+	}
+
+	signal := make(chan string)
+	core.RunPipeline(pipelines["test"], signal)
+
+	select {
+	case <-make(chan bool):
+		break
+	}
 }
