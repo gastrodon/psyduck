5e0ab3bbdbcd3c7f32e58d1a201fa3cf19a52628
Eva <mail@gastrodon.io>
2022-07-29 23:29:30 -0400

    WIP

--- cmd/psyduck/main.go
+++ cmd/psyduck/main.go
@@ -2,43 +2,53 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
-	std "github.com/gastrodon/psyduck-std"
 	"github.com/gastrodon/psyduck/config"
-	"github.com/gastrodon/psyduck/core"
 )
 
+// func main() {
+// 	file := flag.String("file", "psyduck.yml", "File to interpret")
+// 	pipeline := flag.String("pipeline", "", "Pipelines to run")
+// 	flag.Parse()
+
+// 	if *pipeline == "" {
+// 		panic("a value for -pipeline wasn't supplied")
+// 	}
+
+// 	pipelinesRaw, err := config.LoadFile(*file)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	library := core.NewLibrary()
+// 	library.Load(std.IFunny())
+// 	library.Load(std.Psyduck())
+// 	library.Load(std.Scyther())
+
+// 	pipelines := make(core.Pipelines, len(*pipelinesRaw))
+// 	for name, pipelineRaw := range *pipelinesRaw {
+// 		pipeline, err := core.BuildPipeline(pipelineRaw, library)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+
+// 		pipelines[name] = pipeline
+// 	}
+
+// 	signal := make(chan string)
+// 	if err := core.RunPipeline(pipelines[*pipeline], signal); err != nil {
+// 		panic(err)
+// 	}
+// }
+
 func main() {
-	file := flag.String("file", "psyduck.yml", "File to interpret")
-	pipeline := flag.String("pipeline", "", "Pipelines to run")
+	file := flag.String("file", "psyduck.hcl", "File to interpret")
 	flag.Parse()
-
-	if *pipeline == "" {
-		panic("a value for -pipeline wasn't supplied")
-	}
-
-	pipelinesRaw, err := config.LoadFile(*file)
+	pipelines, err := config.LoadFile(*file)
 	if err != nil {
 		panic(err)
 	}
 
-	library := core.NewLibrary()
-	library.Load(std.IFunny())
-	library.Load(std.Psyduck())
-	library.Load(std.Scyther())
-
-	pipelines := make(core.Pipelines, len(pipelinesRaw))
-	for name, pipelineRaw := range pipelinesRaw {
-		pipeline, err := core.BuildPipeline(pipelineRaw, library)
-		if err != nil {
-			panic(err)
-		}
-
-		pipelines[name] = pipeline
-	}
-
-	signal := make(chan string)
-	if err := core.RunPipeline(pipelines[*pipeline], signal); err != nil {
-		panic(err)
-	}
+	fmt.Printf("%#v\n", pipelines.Pipelines["pull-content-raw"].Transformers)
 }
