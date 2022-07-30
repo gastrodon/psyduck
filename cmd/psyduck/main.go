981c480c57842bffc2559a927377a4dda84c0e16
Eva <mail@gastrodon.io>
2022-07-30 13:09:22 -0400

    Use our new hcl parser

--- cmd/psyduck/main.go
+++ cmd/psyduck/main.go
@@ -4,51 +4,42 @@ import (
 	"flag"
 	"fmt"
 
+	std "github.com/gastrodon/psyduck-std"
 	"github.com/gastrodon/psyduck/config"
+	"github.com/gastrodon/psyduck/core"
 )
 
-// func main() {
-// 	file := flag.String("file", "psyduck.yml", "File to interpret")
-// 	pipeline := flag.String("pipeline", "", "Pipelines to run")
-// 	flag.Parse()
+func library() *core.Library {
+	made := core.NewLibrary()
+	made.Load(std.IFunny())
+	made.Load(std.Psyduck())
+	made.Load(std.Scyther())
 
-// 	if *pipeline == "" {
-// 		panic("a value for -pipeline wasn't supplied")
-// 	}
-
-// 	pipelinesRaw, err := config.LoadFile(*file)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	library := core.NewLibrary()
-// 	library.Load(std.IFunny())
-// 	library.Load(std.Psyduck())
-// 	library.Load(std.Scyther())
-
-// 	pipelines := make(core.Pipelines, len(*pipelinesRaw))
-// 	for name, pipelineRaw := range *pipelinesRaw {
-// 		pipeline, err := core.BuildPipeline(pipelineRaw, library)
-// 		if err != nil {
-// 			panic(err)
-// 		}
-
-// 		pipelines[name] = pipeline
-// 	}
-
-// 	signal := make(chan string)
-// 	if err := core.RunPipeline(pipelines[*pipeline], signal); err != nil {
-// 		panic(err)
-// 	}
-// }
+	return made
+}
 
 func main() {
 	file := flag.String("file", "psyduck.hcl", "File to interpret")
+	pipelineTarget := flag.String("pipeline", "", "Pipelines to run")
 	flag.Parse()
-	pipelines, err := config.LoadFile(*file)
+
+	pipelinesConfig, err := config.LoadFile(*file)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%#v\n", pipelines.Pipelines["pull-content-raw"].Transformers)
+	pipelineConfig, ok := pipelinesConfig.Pipelines[*pipelineTarget]
+	if !ok {
+		panic(fmt.Errorf("no such pipeline %s", *pipelineTarget))
+	}
+
+	pipelineBuilt, err := core.BuildPipeline(pipelineConfig, library())
+	if err != nil {
+		panic(err)
+	}
+
+	signal := make(chan string)
+	if err := core.RunPipeline(pipelineBuilt, signal); err != nil {
+		panic(err)
+	}
 }
