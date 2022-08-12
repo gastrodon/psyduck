060fd64368c8837da90e6d72374f032f9fec5741
Eva <mail@gastrodon.io>
2022-08-11 21:45:03 -0400

    Better pipeline runner code

--- cmd/psyduck/main.go
+++ cmd/psyduck/main.go
@@ -1,13 +1,50 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 
 	std "github.com/gastrodon/psyduck-std"
-	"github.com/gastrodon/psyduck/config"
+	"github.com/gastrodon/psyduck/configure"
 	"github.com/gastrodon/psyduck/core"
 )
+
+func done() (func(), chan bool) {
+	chanDone := make(chan bool)
+
+	return func() {
+		chanDone <- true
+	}, chanDone
+}
+
+func do(pipeline *core.Pipeline) error {
+	signal := make(chan string)
+	doneProduce, doneProduceChannel := done()
+	doneConsume, doneConsumeChannel := done()
+
+	chanProduce, chanProduceError := pipeline.Producer(signal, doneProduce)
+	chanConsume, chanConsumeError := pipeline.Consumer(signal, doneConsume)
+
+	for {
+		select {
+		case <-doneProduceChannel:
+			return errors.New("the producer is done (good ending)")
+		case <-doneConsumeChannel:
+			return errors.New("the consumer is done (bad ending)")
+		case err := <-chanProduceError:
+			return err
+		case err := <-chanConsumeError:
+			return err
+		case data := <-chanProduce:
+			if transformed, err := pipeline.StackedTransformer(data); err != nil {
+				return err
+			} else {
+				chanConsume <- transformed
+			}
+		}
+	}
+}
 
 func library() *core.Library {
 	made := core.NewLibrary()
@@ -20,26 +57,24 @@ func library() *core.Library {
 
 func main() {
 	directory := flag.String("where", ".", "Directory with psyduck configs")
-	pipelineTarget := flag.String("pipeline", "", "Pipelines to run")
+	pipelineName := flag.String("pipeline", "", "Pipelines to run")
 	flag.Parse()
 
-	pipelines, context, err := config.LoadDirectory(*directory)
-	if err != nil {
-		panic(err)
+	if *pipelineName == "" {
+		panic(errors.New("flag pipeline requires a value"))
 	}
 
-	pipelineConfig, ok := pipelines.Pipelines[*pipelineTarget]
-	if !ok {
-		panic(fmt.Errorf("no such pipeline %s", *pipelineTarget))
-	}
-
-	pipelineBuilt, err := core.BuildPipeline(pipelineConfig, context, library())
-	if err != nil {
+	if descriptors, context, err := configure.Directory(*directory); err != nil {
 		panic(err)
-	}
-
-	signal := make(chan string)
-	if err := core.RunPipeline(pipelineBuilt, signal); err != nil {
-		panic(err)
+	} else if descriptor, ok := descriptors[*pipelineName]; !ok {
+		panic(fmt.Errorf("can't find pipeline %s", *pipelineName))
+	} else {
+		if pipeline, err := core.BuildPipeline(descriptor, context, library()); err != nil {
+			panic(err)
+		} else {
+			if err := do(pipeline); err != nil {
+				panic(err)
+			}
+		}
 	}
 }
