560fcf9e17d118d57856e1575533bddafcf275a4
Eva <mail@gastrodon.io>
2022-12-12 11:17:11 -0500

    Get rid of Mover

--- sdk/model-provider.go
+++ sdk/model-provider.go
@@ -3,13 +3,10 @@ package sdk
 type Parser func(interface{}) error
 type SpecParser func(SpecMap, interface{}) error
 
-type Mover func(chan string, func()) (chan []byte, chan error)
-type MoverProvider func(func([]byte) error) (Mover, error)
-
-type Producer Mover
+type Producer func(chan string, func()) (chan []byte, chan error)
 type ProducerProvider func(Parser, SpecParser) (Producer, error)
 
-type Consumer Mover
+type Consumer func(chan string, func()) (chan []byte, chan error)
 type ConsumerProvider func(Parser, SpecParser) (Consumer, error)
 
 type Transformer func([]byte) ([]byte, error)
