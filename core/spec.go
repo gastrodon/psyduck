c0b9c9e42b2e3299ed1d3ffa10a5077f38b3eb40
Eva <mail@gastrodon.io>
2022-12-13 09:41:31 -0500

    Axe dead code

--- core/spec.go
+++ core/spec.go
@@ -3,8 +3,6 @@ package core
 import (
 	"github.com/gastrodon/psyduck/sdk"
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hcldec"
-	"github.com/zclconf/go-cty/cty"
 )
 
 func makeBodySchema(specMap sdk.SpecMap) *hcl.BodySchema {
@@ -23,32 +21,4 @@ func makeBodySchema(specMap sdk.SpecMap) *hcl.BodySchema {
 	return &hcl.BodySchema{
 		Attributes: attributes,
 	}
-}
-
-func buildSpecMap(specMap sdk.SpecMap) hcldec.Spec {
-	object := make(hcldec.ObjectSpec, len(specMap))
-	for name, spec := range specMap {
-		object[name] = buildSpec(spec)
-	}
-
-	return object
-}
-
-func buildSpec(spec *sdk.Spec) hcldec.Spec {
-	hclSchema := &hcldec.AttrSpec{
-		Name:     spec.Name,
-		Type:     cty.Type(spec.Type),
-		Required: spec.Required,
-	}
-
-	if spec.Required {
-		return &hcldec.DefaultSpec{
-			Primary: hclSchema,
-			Default: &hcldec.LiteralSpec{
-				Value: spec.Default,
-			},
-		}
-	}
-
-	return hclSchema
 }
