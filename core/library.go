fbb8a63f58e15c1f278bd628222bb53d4338b8f6
Eva <mail@gastrodon.io>
2022-12-13 17:57:26 -0500

    Move makeBodySchema -> library

--- core/library.go
+++ core/library.go
@@ -7,6 +7,24 @@ import (
 	"github.com/hashicorp/hcl/v2"
 )
 
+func makeBodySchema(specMap sdk.SpecMap) *hcl.BodySchema {
+	attributes := make([]hcl.AttributeSchema, len(specMap))
+
+	index := 0
+	for _, spec := range specMap {
+		attributes[index] = hcl.AttributeSchema{
+			Name:     spec.Name,
+			Required: spec.Required,
+		}
+
+		index++
+	}
+
+	return &hcl.BodySchema{
+		Attributes: attributes,
+	}
+}
+
 func makeParser(providedSpecMap sdk.SpecMap, context *hcl.EvalContext, config hcl.Body) (sdk.Parser, sdk.SpecParser) {
 	parser := func(spec sdk.SpecMap, target interface{}) error {
 		content, _, diags := config.PartialContent(makeBodySchema(spec))
