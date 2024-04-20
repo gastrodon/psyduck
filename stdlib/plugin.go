54620d2a0b1945e9c7c4153d5b5dda0858d1cefa
eva <mail@gastrodon.io>
2024-04-19 20:18:19 -0400

    sort stdlib transformers

--- stdlib/plugin.go
+++ stdlib/plugin.go
@@ -63,58 +63,6 @@ func Plugin() *sdk.Plugin {
 				},
 			},
 			{
-				Name:               "transpose",
-				Kinds:              sdk.TRANSFORMER,
-				ProvideTransformer: transform.Transpose,
-				Spec: sdk.SpecMap{
-					"fields": &sdk.Spec{
-						Name:        "fields",
-						Description: "fields to transpose, mapping of target -> source",
-						Type:        cty.Map(cty.List(cty.String)),
-						Required:    true,
-					},
-				},
-			},
-			{
-				Name:               "zoom",
-				Kinds:              sdk.TRANSFORMER,
-				ProvideTransformer: transform.Zoom,
-				Spec: sdk.SpecMap{
-					"field": &sdk.Spec{
-						Name:        "field",
-						Description: "field to zoom into",
-						Type:        cty.String,
-						Required:    true,
-					},
-				},
-			},
-			{
-				Name:               "wait",
-				Kinds:              sdk.TRANSFORMER,
-				ProvideTransformer: transform.Wait,
-				Spec: sdk.SpecMap{
-					"milliseconds": &sdk.Spec{
-						Name:        "milliseconds",
-						Description: "duratin to wait in ms",
-						Type:        cty.Number,
-						Required:    true,
-					},
-				},
-			},
-			{
-				Name:  "increment",
-				Kinds: sdk.PRODUCER,
-				Spec: sdk.SpecMap{
-					"stop-after": &sdk.Spec{
-						Name:        "stop-after",
-						Description: "stop after n iterations",
-						Type:        cty.Number,
-						Default:     cty.NumberIntVal(0),
-					},
-				},
-				ProvideProducer: produce.Increment,
-			},
-			{
 				Name:  "sprintf",
 				Kinds: sdk.TRANSFORMER,
 				Spec: sdk.SpecMap{
@@ -133,6 +81,58 @@ func Plugin() *sdk.Plugin {
 					},
 				},
 				ProvideTransformer: transform.Sprintf,
+			},
+			{
+				Name:               "transpose",
+				Kinds:              sdk.TRANSFORMER,
+				ProvideTransformer: transform.Transpose,
+				Spec: sdk.SpecMap{
+					"fields": &sdk.Spec{
+						Name:        "fields",
+						Description: "fields to transpose, mapping of target -> source",
+						Type:        cty.Map(cty.List(cty.String)),
+						Required:    true,
+					},
+				},
+			},
+			{
+				Name:               "wait",
+				Kinds:              sdk.TRANSFORMER,
+				ProvideTransformer: transform.Wait,
+				Spec: sdk.SpecMap{
+					"milliseconds": &sdk.Spec{
+						Name:        "milliseconds",
+						Description: "duratin to wait in ms",
+						Type:        cty.Number,
+						Required:    true,
+					},
+				},
+			},
+			{
+				Name:               "zoom",
+				Kinds:              sdk.TRANSFORMER,
+				ProvideTransformer: transform.Zoom,
+				Spec: sdk.SpecMap{
+					"field": &sdk.Spec{
+						Name:        "field",
+						Description: "field to zoom into",
+						Type:        cty.String,
+						Required:    true,
+					},
+				},
+			},
+			{
+				Name:  "increment",
+				Kinds: sdk.PRODUCER,
+				Spec: sdk.SpecMap{
+					"stop-after": &sdk.Spec{
+						Name:        "stop-after",
+						Description: "stop after n iterations",
+						Type:        cty.Number,
+						Default:     cty.NumberIntVal(0),
+					},
+				},
+				ProvideProducer: produce.Increment,
 			},
 		},
 	}
