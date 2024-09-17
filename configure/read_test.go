9f95d0a62c1ed46f029f7b384c2c789d72d1899c
eva <mail@gastrodon.io>
2024-09-17 16:22:34 -0400

    make TestLiteral testcases local

--- configure/read_test.go
+++ configure/read_test.go
@@ -6,34 +6,34 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var cases = []struct {
-	Literal  string
-	Filename string
-	Want     map[string]*Pipeline
-}{
-	{
-		Literal: `
-		produce "test" "p" {}
-		consume "test" "c" {}
-		pipeline "test" {
-			produce = [produce.test.p]
-			consume = [consume.test.c]
-			transform = []
-		}
-		`,
-		Filename: "test.psy",
-		Want: map[string]*Pipeline{
-			"test": {
-				Name:         "test",
-				Producers:    []*pipelinePart{{Kind: "test", Name: "p"}},
-				Consumers:    []*pipelinePart{{Kind: "test", Name: "c"}},
-				Transformers: nil,
+func TestLiteral(test *testing.T) {
+	cases := []struct {
+		Literal  string
+		Filename string
+		Want     map[string]*Pipeline
+	}{
+		{
+			Literal: `
+			produce "test" "p" {}
+			consume "test" "c" {}
+			pipeline "test" {
+				produce = [produce.test.p]
+				consume = [consume.test.c]
+				transform = []
+			}
+			`,
+			Filename: "test.psy",
+			Want: map[string]*Pipeline{
+				"test": {
+					Name:         "test",
+					Producers:    []*pipelinePart{{Kind: "test", Name: "p"}},
+					Consumers:    []*pipelinePart{{Kind: "test", Name: "c"}},
+					Transformers: nil,
+				},
 			},
 		},
-	},
-}
+	}
 
-func TestLiteral(test *testing.T) {
 	for i, testcase := range cases {
 		configs, _, err := Literal(testcase.Filename, []byte(testcase.Literal))
 		if err != nil {
