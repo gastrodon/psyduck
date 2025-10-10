bb4ee54671f12595abde7694f1e374924e990efd
eva <mail@gastrodon.io>
2025-10-09 22:48:04 -0400

    yaml parser WIP

---
+++ configure_yaml/types.go
@@ -0,0 +1,26 @@
+// Package configure_yaml defines types for YAML-based pipeline configuration.
+package configure_yaml
+
+// PartYAML represents a pipeline part (producer, consumer, transformer) in YAML format.
+type PartYAML struct {
+	Kind    string                 `yaml:"kind"`
+	Name    string                 `yaml:"name"`
+	Options map[string]interface{} `yaml:",inline"`
+}
+
+// PipelineYAML represents a pipeline configuration in YAML format.
+type PipelineYAML struct {
+	Name         string     `yaml:"name"`
+	Produce      []PartYAML `yaml:"produce,omitempty"`
+	ProduceFrom  string     `yaml:"produce-from,omitempty"`
+	Consumers    []PartYAML `yaml:"consume"`
+	Transformers []PartYAML `yaml:"transform"`
+	StopAfter    int        `yaml:"stop-after,omitempty"`
+	ExitOnError  bool       `yaml:"exit-on-error,omitempty"`
+}
+
+// Config represents the top-level configuration.
+// It supports a sequence of pipelines.
+type Config struct {
+	Pipelines []PipelineYAML `yaml:"pipelines"`
+}
